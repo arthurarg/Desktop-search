50d23f4a33b40e833328ec180d3190878cc0c99c
Arthur <Arthur@Arthur-HP.mshome.net>
2013-04-07 01:01:27 +0200

    Mise en place de l'écriture de l'index

--- projet_info/src/structure/EcritureIndex.java
+++ projet_info/src/structure/EcritureIndex.java
@@ -1,10 +1,14 @@
 package structure;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class EcritureIndex {
 	
 	public static void creation(Stockage s, BTree voc){
 		// creation du dossier mots
+		File f=new File("mots");
+		f.mkdir();
 		
 		Triplet tr;
 		String mot;
@@ -17,6 +21,22 @@ public class EcritureIndex {
 	
 	static void ajouter(String mot, Triplet tr){
 		// on ajoute au fichier "mot" le couple (d, f)
+		try{
+			FileWriter file = new FileWriter("mots/"+mot+".txt", true);
+			file.write(tr.d+" "+tr.f+" ");
+			file.close();
+		}
+		catch(IOException e){
+			System.out.println("erreur lors de l'ajout de "+mot+" à l'index");
+		}
+	}
+	
+	public static void main(String[] args){
+		File f=new File("mots");
+		if(f.mkdir())
+			System.out.println("OK");
+		else System.out.println("NO");
+		ajouter("bite", new Triplet(1, 8,9));
 	}
 
 }
